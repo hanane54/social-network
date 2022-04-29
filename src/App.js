5b908fe23b8a6c53bbb42ff3c11824fba59cf16c
Najwa983 <Najwa.lakmouri1@gmail.com>
2022-04-29 19:05:10 +0100

    adding the menu bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import './App.css';
 import React from 'react';
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/header/header"
+import Menu from "./components/menu/menu";
 
 function App() {
   return (
     <>
       <Header />
-      <div style={{paddingTop:'64px', backgroundColor:'#F7F5F2'}}>
+
+      <div style={{ paddingTop: '64px', backgroundColor: '#F7F5F2' }}>
+        <Menu></Menu>
         <Routes>
-        <Route path="/" exact element={<h2>this is the home page</h2>}/>
-        <Route path="/user-settings" exact element={<h2>this is the home page</h2>}/>
-        <Route path="/post" exact element={<h2>this is the home page</h2>}/>
-        <Route path="/publish-post" exact element={<h2>this is the home page</h2>}/>
-        <Route path="/login" exact element={<h2>this is the home page</h2>}/>
-        <Route path="/signup" exact element={<h2>this is the home page</h2>}/>
-        <Route path="/myprofile" exact element={<h2>this is the home page</h2>}/>
-        <Route path="/user" exact element={<h2>this is the home page</h2>}/>
-      </Routes>
+          <Route path="/" exact element={<h2>this is the home page</h2>} />
+          <Route path="/user-settings" exact element={<h2>this is the home page</h2>} />
+          <Route path="/post" exact element={<h2>this is the home page</h2>} />
+          <Route path="/publish-post" exact element={<h2>this is the home page</h2>} />
+          <Route path="/saved-post" exact element={<h2>this is the home page</h2>} />
+          <Route path="/login" exact element={<h2>this is the home page</h2>} />
+          <Route path="/signup" exact element={<h2>this is the home page</h2>} />
+          <Route path="/myprofile" exact element={<h2>this is the home page</h2>} />
+          <Route path="/user" exact element={<h2>this is the home page</h2>} />
+        </Routes>
       </div>
-      
+
     </>
   );
 }
