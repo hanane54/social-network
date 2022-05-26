d3c754107af5a94ed86aa49fb1cc6aa2161ce127
hanane54 <hanane59taouil@gmail.com>
2022-05-26 12:30:27 +0100

    Added: mongoDB configuration files, connected app to the mongoDB, initialized the models folder

--- server/index.js
+++ server/index.js
@@ -1,14 +1,28 @@
 const express = require("express");
-
-const PORT = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
 const app = express();
+
+// DB Connection
+mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true})
+.then( () => {
+  console.log('DB Connected');
+})
+.catch( (err) => {
+  console.log(err);
+});
+
+
+
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+  console.log(`Server listening on ${PORT}`);
+});
 
 // here we can add methods that can perform a mission (send a response) when we access a route.
-// it should be above the app.listen 
+// it should be above the app.listen
 // app.get("/api", (req, res) => {
 //     res.json({ message: "Hello from server!" });
 //   });
